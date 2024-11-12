77145d00bf6706ce9dcad441aaffdbbba5d004ca
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2024-11-12 13:17:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const makeChange = (c) => {
   // your name here
+	let result = {
+    q: Math.floor(c / 25),
+    d: Math.floor((c % 25) / 10),
+    n: Math.floor((c % 25 % 10) / 5),
+    p: c % 25 % 10 % 5
+  };
+  return result;
 };
 
 // Do not the change the code below
